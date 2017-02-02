b2c1c2608f1b56ea8511963643967a21a5f447e5
Scott Sorensen <sorensen@udel.edu>
2017-02-02 12:04:23 -0500

    Removed old files
    Getting rid of some older files that were hold overs from the old
    machine and scripts I was playing with.

--- FastSegmentation/christianSeg.m
+++
@@ -1,9 +0,0 @@
-function mask = christianSeg( image )
-%CUSTOMSEGMENT Summary of this function goes here
-%   Detailed explanation goes here
-rthresh = 0;
-gthresh = 4;
-bthresh = 4;
-mimage = minusmin(image);
-mask = filterbw2(customThresh(mimage,[rthresh,gthresh,bthresh]));
-end
