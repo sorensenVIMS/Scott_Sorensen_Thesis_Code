5b657a134bce9ca61c57ee7c7d7eb0a6d6044118
Scott Sorensen <sorensen@udel.edu>
2017-02-05 16:36:52 -0500

    Fighting with Github Desktop
    Removed somethings so it wouldn’t crash…
    slowly going to add them back and see if I can fix it.

--- FastSegmentation/algaeSegment.m
+++
@@ -1,9 +0,0 @@
-function mask = algaeSegment( image )
-%CUSTOMSEGMENT Summary of this function goes here
-%   Detailed explanation goes here
-rthresh = 9;
-gthresh = 7;
-bthresh = 0;
-mimage = minusmin(image);
-mask = filterBW(customThresh(mimage,[rthresh,gthresh,bthresh]));
-end
--- FastSegmentation/minusmin.m
+++
@@ -1,8 +0,0 @@
-function subMin = minusmin( image )
-%This function takes an image and computes the minimum color channel and
-%then subtracts every channel from this minimum
-minpic = min(image,[],3);
-subMin = image - cat(3,minpic,minpic,minpic);
-
-end
-
